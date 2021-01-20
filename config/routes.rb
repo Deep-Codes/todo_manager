07494653c956c2aaf983bcc68afee64b64aba654
Deepankar <61158210+Deep-Codes@users.noreply.github.com>
2021-01-20 09:13:02 +0530

    Add User Sign-In Fuctionality

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
   resources :users
 
   get "/" => "home#index"
+  get "/signin" => "sessions#new", as: :new_sessions
+  post "/signin" => "sessions#create", as: :sessions
 
   post "users/login", to: "users#login"
 end
